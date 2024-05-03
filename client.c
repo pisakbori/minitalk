5543099b0fa5e3a0c29406f9ca4a73f2d6fecd4a
bpisak-l <bpisak-l@student.42.fr>
2024-05-03 11:38:35 +0200

    Send only on char

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: bpisak-l <bpisak-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 15:31:23 by bpisak-l          #+#    #+#             */
-/*   Updated: 2024/05/01 15:55:58 by bpisak-l         ###   ########.fr       */
+/*   Updated: 2024/05/03 11:37:11 by bpisak-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,52 @@
 
 // int globalVariable;
 
-int main(int argc, char const *argv[])
+void	send_char(char c, pid_t server_pid)
+{
+	char	bit;
+	char	i;
+
+	i = 8;
+	ft_printf("sending char %d\n", c);
+	while (i--)
+	{
+		bit = c & 0b10000000;
+		ft_printf("bit: %d\n", !!(c & 0b10000000));
+		// ft_printf("%d", !!(c & 0b10000000));
+		if (!bit)
+			kill(server_pid, SIGUSR1);
+		else
+			kill(server_pid, SIGUSR2);
+		usleep(100);
+		c = c << 1;
+	}
+}
+
+
+int	args_valid(int argc, char const *argv[], char **msg)
+{
+	if (argc != 3)
+	{
+		// ft_printf("Correct usage: ./client <server pid> <message string>\n");
+		// return (0);
+		return (1);
+	}
+	*msg = ft_strdup(argv[2]);
+	return (1);
+}
+
+int	main(int argc, char const *argv[])
 {
 	pid_t	server_pid;
 	char	*msg;
 
-	if (argc != 3)
-	{
-		ft_printf("Correct usage: ./client <server pid> <message string>\n");
+	msg = NULL;
+	if (!args_valid(argc, argv, &msg))
 		return (1);
-	}
-	server_pid = ft_atoi(argv[1]);
-	msg = ft_strdup(argv[2]);
+	server_pid = (pid_t)ft_atoi(argv[1]);
+	msg = ft_strdup("heya hey");
+	send_char(msg[0], server_pid);
 	free(msg);
-	kill(server_pid, SIGUSR1);
+	// kill(server_pid, SIGUSR1);
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,28 +6,64 @@
 /*   By: bpisak-l <bpisak-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 15:31:16 by bpisak-l          #+#    #+#             */
-/*   Updated: 2024/05/01 15:57:20 by bpisak-l         ###   ########.fr       */
+/*   Updated: 2024/05/03 11:38:07 by bpisak-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int globalVariable;
+unsigned char g_res;
 
-void sig_handler(int signum){
-  printf("\nInside handler function, %d\n", signum);
-//   signal(SIGINT, SIG_DFL);   // Re Register signal handler for default action
+void	handler(int signum)
+{
+	g_res = g_res << 1;
+	if (signum == SIGUSR1)
+	{
+		write(1, "0", 1);
+	}
+	if (signum == SIGUSR2)
+	{
+		write(1, "1", 1);
+		g_res = g_res + 1;
+	}
 }
 
 // int	main(int argc, char const *argv[])
 int	main(void)
 {
+	// struct sigaction	sa1;
+	// struct sigaction	sa2;
+
+	int	i;
 	pid_t pid = getpid();
-	signal(SIGUSR1, sig_handler);
-    printf("%d\n", pid);
-	while (1)
+	printf("%d\n", pid);
+	// sa1.sa_handler = sig_0;
+	// sa1.sa_flags = SA_SIGINFO;
+	// sa2.sa_handler = sig_1;
+	// sa2.sa_flags = SA_SIGINFO;
+
+	// if (sigaction(SIGUSR1, &sa1, NULL) == -1) 
+	// {
+	// 	perror("sigaction1");
+	// 	return (EXIT_FAILURE);
+	// }
+	// if (sigaction(SIGUSR2, &sa2, NULL) == -1) 
+	// {
+	// 	perror("sigaction2");
+	// 	return (EXIT_FAILURE);
+	// }
+	i = 8;
+	g_res = 0;
+	ft_printf("g_res at start: %d\n", g_res);
+	signal(SIGUSR1, handler);
+	signal(SIGUSR2, handler);
+	usleep(100);
+	while (i--)
 	{
 		pause();
 	}
+	write(1, "\n", 1);
+	ft_printf("g_res in the end: %d\n", g_res);
+	ft_printf("%c\n" ,g_res);
 	return (0);
 }
